801918e039a4d4705ebf1c01a6f9a76a161d896f
rugaheedori <qufslarkgml@gmail.com>
2021-11-10 12:34:54 +0900

    gahee : socket을 활용하여 server에 문자열 전송하고 중복된 문자가 삭제된 결과값 받기

--- socket/client.py
+++ socket/client.py
@@ -1,27 +1,28 @@
 import socket
 from time import sleep
 
-testSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
 ip = "127.0.0.1"
-port = 9999
+port = 8080
 
 bConn = 0
 while not bConn:
     try:
-        print("> Connecting to the server ...")
-        testSocket.connect((ip, port))
+        print("*Connecting to the server - !")
+        clientSocket.connect((ip, port))
         sleep(0.5)
         bConn = 1
     except socket.error as e:
         print("Error/Socket "+str(e))
         sleep(1)
         continue
-print("> Server connected!!")
+print("* Server connected - !")
 
 while 1:
-    no = int(input("> Data to send to the server? "))
-    testSocket.send(no.to_bytes(1, 'big'))
+    text = input("# Data to send to the server?")
+    clientSocket.send(text.encode())
     print("> Waiting for data from the server...")
-    msg = testSocket.recv(1)
-    print("> Received data from client =", int.from_bytes(msg, "big"))
+    dataFromServer = clientSocket.recv(1024)
+    print("> Received data from client =", dataFromServer.decode())
